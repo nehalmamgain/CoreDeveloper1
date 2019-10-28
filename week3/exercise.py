71da2aac0989cea10373a3660efa458671e63cf4
nehalmamgain <36276997+nehalmamgain@users.noreply.github.com>
2019-10-28 19:23:18 +0900

    Update exercise.py

--- week3/exercise.py
+++ week3/exercise.py
@@ -8,6 +8,41 @@
 
 # -----FIZZBUZZ CODE HERE-----
 
+def Fizz(number):
+   '''
+   Returns true if the number is divisible by 3
+   '''
+   return (False,True)[number % 3 == 0]
+
+def Buzz(number):
+   '''
+   Returns true if the number is divisible by 5
+   '''
+   return (False,True)[number % 5 == 0]
+
+def FizzBuzz(number):
+  '''
+  Brief: Checks for divisibility of the number by 3, 5 and 15.
+  Details:
+  1. Prints number itself if the number is not divisible by 3 and not divisible by 5.
+  2. Prints "Fizz " if the number is divisible by 3.
+  3. Prints "Buzz " if the number is divisible by 5.
+  4. Prints "Fizz Buzz " if the number is divisible by 15.
+  '''
+   if not Fizz(number) and not Buzz(number):
+       print(number, end=' ')
+       return
+   if Fizz(number):
+       print("Fizz", end=' ')
+   if Buzz(number):
+       print("Buzz", end=' ')
+
+if __name__ == "__main__":
+   '''
+   Main function. Uses function FizzBuzz() to check for divisibility of numbers 0 to 100 (both inclusive).
+   '''
+   for number in range(101):
+       FizzBuzz(number)
 
 # -----END FIZBUZZ CODE-----
 
